ecd0be5492d080da582f3127b77afb3581bb33c5
Parv Gupta <parvg555@gmail.com>
2021-07-08 22:00:49 +0530

    complete chatbot

--- app.js
+++ app.js
@@ -1,6 +1,93 @@
 const express = require('express');
+const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
+const cors = require('cors');
 
 const app = express();
 
-app.set('view engine','ejs');
+const sessionId = uuid.v4();
 
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+//enabling CORS
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.post('/send-msg', (req,res) =>{
+
+    runSample(req.body.MSG).then(data =>{
+        res.send({Reply: data})
+    })
+});
+
+app.get('/',(req,res) => {
+    res.render('index');
+});
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(MSG,projectId = 'rn-bot-lngc') {
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+      //here have to change the path 
+      keyFilename:"C:/Users/Parv/Desktop/ChatBot-NodeJs/key2.json",
+  });
+  const sessionPath = sessionClient.projectAgentSessionPath(
+    projectId,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: MSG,
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log('  No intent matched.');
+  }
+  return result.fulfillmentText;
+}
+
+app.listen(3000);
